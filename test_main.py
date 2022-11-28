ddcdd6083075a4f3357d35e44112ea4da6e770f7
Babou-Dev <baptiste.oger@icloud.com>
2022-11-28 16:22:17 +0100

    ⚙ changed test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import main
 
 
-# tests sur get basiques
+# tests partie 1 : get basiques
 def test_get_host():
     """ Test unitaire pour get_host """
     assert main.get_host("Moi JJ HH:MM:SS host program[process_id]: message") == "host"
@@ -49,7 +49,7 @@ def test_get_complete_date():
     assert main.get_complete_date("Moi JJ HH:MM:SS host program[10]: message") == "Moi JJ HH:MM:SS"
 
 
-# tests sur dates
+# tests partie 2 : dates
 def test_formated_date_january():
     """ Test unitaire pour formated_date """
     assert main.formated_date("Jan 18 22:30:15") == "01:18 22:30:15"
@@ -110,93 +110,112 @@ def test_formated_date_december():
     assert main.formated_date("Dec 18 22:30:15") == "12:18 22:30:15"
 
 
-L_TEST = [
+L_TEST_DATE = [
     "Jan 13 22:30:15 blabla",
     "Jan 14 23:30:15 blabla",
     "Jan 16 26:30:15 blabla",
     "Jan 14 24:30:15 blabla",
-    "Jan 15 25:30:15 blabla",
+    "Jan 15 25:30:15 blabla"
 ]
 
 
 def test_logs_by_day1():
     """ Test unitaire pour logs_by_day """
-    output = main.logs_by_day(L_TEST, "Jan 14")
+    output = main.logs_by_day(L_TEST_DATE, "Jan 14")
+    expected = [
+        "Jan 14 23:30:15 blabla",
+        "Jan 14 24:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 14 23:30:15 blabla",
-            "Jan 14 24:30:15 blabla"]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
 def test_logs_by_day2():
     """ Test unitaire pour logs_by_day """
-    output = main.logs_by_day(L_TEST, "Jan 13")
-    for elem in output:
-        assert elem in ["Jan 13 22:30:15 blabla"]
+    output = main.logs_by_day(L_TEST_DATE, "Jan 13")
+    expected = ["Jan 13 22:30:15 blabla"]
+    assert output == expected
 
 
 def test_logs_between():
     """ Test unitaire pour logs_between """
-    output = main.logs_between(L_TEST, "01:14 24:29:15", "01:16 26:31:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:29:15", "01:16 26:31:15")
+    expected = [
+        "Jan 14 24:30:15 blabla",
+        "Jan 15 25:30:15 blabla",
+        "Jan 16 26:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 14 24:30:15 blabla",
-            "Jan 15 25:30:15 blabla",
-            "Jan 16 26:30:15 blabla"
-        ]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
 def test_logs_between_limite_min():
     """ Test unitaire pour logs_between (min limite) """
-    output = main.logs_between(L_TEST, "01:14 24:30:15", "01:16 26:29:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:30:15", "01:16 26:29:15")
+    expected = [
+        "Jan 14 24:30:15 blabla",
+        "Jan 15 25:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 14 24:30:15 blabla",
-            "Jan 15 25:30:15 blabla",
-        ]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
 def test_logs_between_limite_max():
     """ Test unitaire pour logs_between (max limite) """
-    output = main.logs_between(L_TEST, "01:14 24:29:15", "01:16 26:30:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:29:15", "01:16 26:30:15")
+    expected = [
+        "Jan 14 24:30:15 blabla",
+        "Jan 15 25:30:15 blabla",
+        "Jan 16 26:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 14 24:30:15 blabla",
-            "Jan 15 25:30:15 blabla",
-            "Jan 16 26:30:15 blabla"
-        ]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
 def test_logs_between_default_min():
     """ Test unitaire pour logs_between (min par défaut) """
-    output = main.logs_between(L_TEST, date_max="01:14 23:40:00")
+    output = main.logs_between(L_TEST_DATE, date_max="01:14 23:40:00")
+    expected = [
+        "Jan 13 22:30:15 blabla",
+        "Jan 14 23:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 13 22:30:15 blabla",
-            "Jan 14 23:30:15 blabla"
-        ]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
 def test_logs_between_default_max():
     """ Test unitaire pour logs_between (max par défaut) """
-    output = main.logs_between(L_TEST, date_min="01:14 23:40:00")
+    output = main.logs_between(L_TEST_DATE, date_min="01:14 23:40:00")
+    expected = [
+        "Jan 16 26:30:15 blabla",
+        "Jan 14 24:30:15 blabla",
+        "Jan 15 25:30:15 blabla"
+    ]
     for elem in output:
-        assert elem in [
-            "Jan 16 26:30:15 blabla",
-            "Jan 14 24:30:15 blabla",
-            "Jan 15 25:30:15 blabla",
-        ]
+        assert elem in expected
+    for elem in expected:
+        assert elem in output
 
 
-# tests partie 3 :
+# tests parties 3 :
 L_TEST_3 = [
     "Oct JJ HH:MM:SS host programA[1]: blabla",
     "Nov JJ HH:MM:SS host programA[2]: bla: bla: Error404",
-    "Dec JJ HH:MM:SS host programB[5]: message d'error",
     "Jan JJ HH:MM:SS host programA[2]: oups...",
     "Feb JJ HH:MM:SS host programB[5]: FATAL ERRoR",
     "Mar JJ HH:MM:SS host program: message",
     "Mar JJ HH:MM:SS host program: olala eRRoR",
+    "Dec JJ HH:MM:SS host programB[5]: message d'error",
     "Apr JJ HH:MM:SS host programA[42]: J'aime les fruits au sirop",
     "May JJ HH:MM:SS host programA[42]: La progra c'est génial ! Youpie !",
     "Jun JJ HH:MM:SS host programA[42]: MeSsAgE",
@@ -330,6 +349,7 @@ def test_suspects():
     """ Test unitaire pour suspects """
     output = main.suspects(L_TEST_3, 1)
     expected = ["programB", "program"]
+    assert len(output) == 2
     for elem in output:
         assert elem in expected
     for elem in expected:
